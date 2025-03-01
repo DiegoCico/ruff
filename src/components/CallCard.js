cebe415d108128561a74fcf9e72e3fb3e5ada4d5
Diego Cicotoste <cicotosted@gmail.com>
2025-03-01 12:30:59 -0500

    test again
    Co-authored-by: Yura Hriaziev <yura.hriaziev@gmail.com>

--- src/components/CallCard.js
+++ src/components/CallCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import '../css/CallCard.css'
 
-export default function CallCard() {
+export default function CallCard({ date, time, duration}) {
     return (
         <div className='call-card'>
             <div className='card-details'>
-                
+            <h4><span className="material-icons">event</span> {date}</h4>
+                <p><span className="material-icons">schedule</span> {time} • {duration} min</p>
+                <p className="transcript-link"><span className="material-icons">description</span> Get transcript of call</p>
             </div>
         </div>
     )
-}
+}   
