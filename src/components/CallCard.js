fd7fd75e05795dc8f2fb724bd894afeff432bd72
Diego Cicotoste <cicotosted@gmail.com>
2025-03-01 12:29:30 -0500

    test Co-authored-by: yurahriaziev <yura.hriaziev@gmail.com>

---
+++ src/components/CallCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function CallCard() {
+    return (
+        <div className='call-card'>
+            <div className='card-details'>
+                
+            </div>
+        </div>
+    )
+}
